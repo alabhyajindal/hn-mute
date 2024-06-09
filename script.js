6170f11d1a98eada42e7b5a3135c2ec57c06fa3f
Alabhya Jindal <alabhya10@gmail.com>
2024-06-09 14:53:33 +0530

    comments

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
 function main(muted_words) {
   const pagespace = document.querySelector('#pagespace')
+  // The next element after pagespace is a table row
   const tableRow = pagespace.nextElementSibling
   const submissions = Array.from(tableRow.querySelectorAll('.titleline > a'))
-  console.log(submissions)
   const tableBody = tableRow.querySelector('tbody')
 
+  // A spacer is present after every submission
   const spacerElem = document.querySelector('.spacer')
+
+  // Last two elements in the table body are responsible for the "More" link that goes to the next page
   const morespace = tableBody.children[tableBody.children.length - 2]
   const more = tableBody.children[tableBody.children.length - 1]
 
+  // Create a new table body that will eventually replace the original
   const newTableBody = document.createElement('tbody')
+  // Variable used to specify the rank in submissions
   let count = 0
 
   submissions.forEach((submission) => {
+    // Case insensitive matching
     const title = submission.innerHTML.toLowerCase()
+    // true if the submission contains a word that's muted
     const wordInSubmission = muted_words.some((word) => {
       const re = new RegExp(`\\b${word}\\b`)
       return title.match(re)
     })
 
+    // ok submissions, ie, the title doesn't contain a muted word
     if (!wordInSubmission) {
+      // Element with the upvote button, title and url
       const titleParent = submission.closest('tr')
+
+      // Element with the points, time and other details
       const descParent = titleParent.nextElementSibling
+
+      // Updating the rank so the final list are in order
       const rank = titleParent.querySelector('.rank')
       rank.innerHTML = ++count
 
-      newTableBody.appendChild(titleParent.cloneNode(true))
-      newTableBody.appendChild(descParent.cloneNode(true))
-      newTableBody.appendChild(spacerElem.cloneNode(true))
+      // Appending to the new table body
+      newTableBody.appendChild(titleParent)
+      newTableBody.appendChild(descParent)
+      // spacerElem is cloned because we want a new element to be added on each iteration
+      newTableBody.appendChild(spacerElem.cloneNode(false))
     }
   })
 
+  // Add valid submissions are added to the new table body at this point
+
+  // Adding the elements that create the "More" link
   newTableBody.appendChild(morespace)
   newTableBody.appendChild(more)
+
+  // Finally replacing the current table children with the new table
   tableBody.replaceChildren(newTableBody)
 }
 
+// Get the person's preference from storage
 const getOptions = browser.storage.sync.get('words')
 
 getOptions.then(
